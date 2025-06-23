a3677372e28df5da4b3cec8fbdbbf2fed6577e75
alicemarple <itachitsukuyomi46@gmail.com>
2025-06-23 21:53:18 +0530

    feat(constants): Add separate constants file

--- internal/constants/constant.go
+++ internal/constants/constant.go
@@ -1,10 +1,17 @@
 package constants
 
+import (
+	"os"
+	"path/filepath"
+)
+
 var (
-	BaseURL   = "https://github.com"
-	SyncURl   = "https://api.github.com/repos"
-	SyncFile  = "/mnt/e/projects/golang/lazydot/sync/sync.yml"
-	LoaclFile = "/mnt/e/projects/golang/lazydot/local/local.yml"
-	// DownloadLocation = "/mnt/e/"
-	DownloadLocation = "/mnt/e/projects/golang/lazydot/"
+	BaseURL    = "https://github.com"
+	SyncURl    = "https://api.github.com/repos"
+	LocalFile  = filepath.Join(os.Getenv("HOME"), ".local", "share", "lazydot", "local.yml")
+	SyncFile   = filepath.Join(os.Getenv("HOME"), ".local", "share", "lazydot", "sync.yml")
+	ConfigFile = filepath.Join(os.Getenv("HOME"), ".config", "lazydot", "config.yml")
+
+	// ConfigFile       = "/mnt/e/projects/golang/lazydot/config/config.yml"
+	DownloadLocation = filepath.Join(os.Getenv("HOME"), ".cache", "lazydot", "pkg")
 )
