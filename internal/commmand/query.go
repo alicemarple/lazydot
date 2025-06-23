56db0b7bdbbcf63a4788b88b493aabbcdc795495
alicemarple <itachitsukuyomi46@gmail.com>
2025-06-23 21:54:10 +0530

    feat(query): Add query to get local info

--- internal/commmand/query.go
+++ internal/commmand/query.go
@@ -1,7 +1,57 @@
 package commmand
 
-import "fmt"
+import (
+	"fmt"
 
+	"github.com/alicemarple/lazydot/internal/constants"
+	"github.com/alicemarple/lazydot/pkg/model"
+	"github.com/alicemarple/lazydot/pkg/util/file"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
+)
+
+// Query function
 func Query() {
-	fmt.Println("query the database")
+	// Read data from the YAML
+	dt := file.ReadYml[model.MetaData](constants.LocalFile)
+
+	printQueryTable(dt)
+}
+
+func printQueryTable(dt []model.MetaData) {
+	// Styles
+	var (
+		purple    = lipgloss.Color("99")
+		gray      = lipgloss.Color("245")
+		lightGray = lipgloss.Color("241")
+
+		headerStyle  = lipgloss.NewStyle().Foreground(purple).Bold(true).Align(lipgloss.Center)
+		cellStyle    = lipgloss.NewStyle().Padding(0, 1).Width(20)
+		oddRowStyle  = cellStyle.Foreground(gray)
+		evenRowStyle = cellStyle.Foreground(lightGray)
+	)
+
+	// Build table
+	t := table.New().
+		Border(lipgloss.NormalBorder()).
+		BorderStyle(lipgloss.NewStyle().Foreground(purple)).
+		StyleFunc(func(row, col int) lipgloss.Style {
+			switch {
+			case row == table.HeaderRow:
+				return headerStyle
+			case row%2 == 0:
+				return evenRowStyle
+			default:
+				return oddRowStyle
+			}
+		}).
+		Headers("Name", "Version", "Destination")
+
+	// Add rows
+	for _, v := range dt {
+		t.Row(v.Name, v.Version, v.ConfDir)
+	}
+
+	// Print the table
+	fmt.Println(t)
 }
