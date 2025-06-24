553bbff62430f0b2be2ac1c5b7bc6efb3862e567
alicemarple <itachitsukuyomi46@gmail.com>
2025-06-24 13:21:16 +0530

    feat(model): Add interface for print function

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,8 +1,30 @@
 package model
 
+// interface
+// Data
 type Data interface {
 	GetName() string
 }
+
+func (m MetaData) GetName() string { return m.Name }
+func (c Config) GetName() string   { return c.Name }
+func (s SyncData) GetName() string { return s.Name }
+
+type PrintData interface {
+	GetPName() string
+	GetPVersion() string
+	GetPConfDir() string
+}
+
+// methods for MetaData
+func (m MetaData) GetPName() string    { return m.Name }
+func (m MetaData) GetPVersion() string { return m.Version }
+func (m MetaData) GetPConfDir() string { return m.ConfDir }
+
+// methods for SyncData
+func (s SyncData) GetPName() string    { return s.Name }
+func (s SyncData) GetPVersion() string { return s.Version }
+func (s SyncData) GetPConfDir() string { return s.ConfDir }
 
 // MetaData struct
 type MetaData struct {
@@ -22,7 +44,3 @@ type SyncData struct {
 	Version string `yaml:"version"`
 	ConfDir string `yaml:"confdir"`
 }
-
-func (m MetaData) GetName() string { return m.Name }
-func (c Config) GetName() string   { return c.Name }
-func (s SyncData) GetName() string { return s.Name }
